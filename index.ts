98f21a5df53d089d419e5d1ace73a414fc921dd5
Atanu Debnath <playatanu@gmail.com>
2023-06-06 19:11:23 +0530

    user route + controller add 🍳

---
+++ index.ts
@@ -0,0 +1,10 @@
+import app from './src/app';
+import 'dotenv/config';
+
+import db from './src/config/db.config';
+const DB_URI = process.env.DB_URI || 'mongodb://127.0.0.1:27017/pizza';
+
+db.connect(DB_URI, () => console.log('database connected'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`server running at port ${PORT}`));
